aac83b2e4ad5183c869726f95a7f12c0c09227fa
António Nuno Monteiro <anmonteiro@gmail.com>
2015-11-21 17:34:54 +0100

    track .emacs.d

---
+++ .emacs.d/customizations/misc.el
@@ -0,0 +1,15 @@
+;; Changes all yes/no questions to y/n type
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; shell scripts
+(setq-default sh-basic-offset 2)
+(setq-default sh-indentation 2)
+
+;; No need for ~ files when editing
+(setq create-lockfiles nil)
+
+;; Go straight to scratch buffer on startup
+(setq inhibit-startup-message t)
+
+;; magit emacsclient
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
---
+++ .emacs.d/customizations/setup-neotree.el
@@ -0,0 +1,42 @@
+;; Neotree
+;; https://github.com/jaypei/emacs-neotree
+
+;;  every time when the neotree window is  opened, it will try to find current
+;;  file and jump to node.
+;;(setq-default neo-smart-open t)
+
+;; Don't allow neotree to be the only open window
+(setq-default neo-dont-be-alone t)
+
+
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+
+(setq neo-theme 'nerd) ; 'classic, 'nerd, 'ascii, 'arrow
+
+(setq neo-vc-integration '(face char))
+
+;; Patch to fix vc integration
+;; (defun neo-vc-for-node (node)
+;;   (let* ((backend (vc-backend node))
+;;          (vc-state (when backend (vc-state node backend))))
+;;     ;; (message "%s %s %s" node backend vc-state)
+;;     (cons (cdr (assoc vc-state neo-vc-state-char-alist))
+;;           (cl-case vc-state
+;;             (up-to-date       neo-vc-up-to-date-face)
+;;             (edited           neo-vc-edited-face)
+;;             (needs-update     neo-vc-needs-update-face)
+;;             (needs-merge      neo-vc-needs-merge-face)
+;;             (unlocked-changes neo-vc-unlocked-changes-face)
+;;             (added            neo-vc-added-face)
+;;             (removed          neo-vc-removed-face)
+;;             (conflict         neo-vc-conflict-face)
+;;             (missing          neo-vc-missing-face)
+;;             (ignored          neo-vc-ignored-face)
+;;             (unregistered     neo-vc-unregistered-face)
+;;             (user             neo-vc-user-face)
+;;             (t                neo-vc-default-face)))))
+
+
+(provide 'setup-neotree)
