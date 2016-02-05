9081720a83e19109475c8d4ef6e6fd95ed808929
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-05 13:36:10 +0100

    setup lein profiles properly for use with cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,3 @@
-{:repl {:plugins [[cider/cider-nrepl "0.9.1"]]}}
+{:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
+ :dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]}}
