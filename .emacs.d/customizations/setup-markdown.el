9adf664a17bd0cead990992e8ded06f8d0de6ebc
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-08 12:15:32 +0100

    wrap lines in markdown mode

--- .emacs.d/customizations/setup-markdown.el
+++ .emacs.d/customizations/setup-markdown.el
@@ -1,3 +1,9 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-hook 'markdown-mode-hook
+          (lambda ()
+            (setq fci-handle-truncate-lines nil)
+            (setq truncate-partial-width-windows nil)
+            (setq-default truncate-lines nil)))
