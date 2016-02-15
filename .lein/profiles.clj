cfb88429883a74378e4d85a0a3bfc8d2fe074c97
António Nuno Monteiro <anmonteiro@gmail.com>
2016-02-15 13:54:53 +0100

    remove `:repl` profile, cider now injects needed dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,1 @@
-{:repl {:plugins [[cider/cider-nrepl "0.11.0-SNAPSHOT"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}
- :dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]}}
+{:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]}}
