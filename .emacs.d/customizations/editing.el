3eefeacda22493c0198f38aa5c7803161b744b27
António Nuno Monteiro <anmonteiro@gmail.com>
2016-03-13 17:11:16 +0100

    set fill-column to col 80

--- .emacs.d/customizations/editing.el
+++ .emacs.d/customizations/editing.el
@@ -99,5 +99,7 @@
 (setq-default truncate-lines t)
 
 ;; display a vertical rule at column 80
+(setq-default fill-column 80)
+
 (require 'fill-column-indicator)
 (add-hook 'after-change-major-mode-hook 'fci-mode)
