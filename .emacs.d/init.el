c07bda7d4131d70d7a42a41daf3f04a0bd1a4e9e
António Nuno Monteiro <anmonteiro@gmail.com>
2016-05-15 15:21:36 +0200

    turn menu-bar back on

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,8 +3,8 @@
 ;;;;
 
 ;; Turn off mouse interface early in startup to avoid momentary display
-(when (fboundp 'menu-bar-mode)
-  (menu-bar-mode -1))
+;; (when (fboundp 'menu-bar-mode)
+;;   (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 ;;(when (fboundp 'scroll-bar-mode)
