0206096c53688a36e7320078223171dba81e0c2e
António Nuno Monteiro <anmonteiro@gmail.com>
2016-07-03 17:34:20 +0100

    truncate lines in latex files

--- .emacs.d/customizations/setup-latex.el
+++ .emacs.d/customizations/setup-latex.el
@@ -8,3 +8,14 @@
 (add-hook 'LaTeX-mode-hook 'enable-paredit-mode)
 (add-hook 'tex-mode-hook 'enable-paredit-mode)
 (add-hook 'TeX-mode-hook 'enable-paredit-mode)
+
+(defun truncate-lines ()
+  (setq fci-handle-truncate-lines nil)
+  (setq truncate-partial-width-windows nil)
+  (setq truncate-lines nil))
+
+;; truncate lines when editing LaTeX
+(add-hook 'latex-mode-hook 'truncate-lines)
+(add-hook 'LaTeX-mode-hook 'truncate-lines)
+(add-hook 'tex-mode-hook 'truncate-lines)
+(add-hook 'TeX-mode-hook 'truncate-lines)
