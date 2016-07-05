3c0524994e244c9cbe35fb7883c5735c3a222043
António Nuno Monteiro <anmonteiro@gmail.com>
2016-07-05 18:11:11 +0100

    add hooks for bibtex-mode

--- .emacs.d/customizations/setup-latex.el
+++ .emacs.d/customizations/setup-latex.el
@@ -8,6 +8,7 @@
 (add-hook 'LaTeX-mode-hook 'enable-paredit-mode)
 (add-hook 'tex-mode-hook 'enable-paredit-mode)
 (add-hook 'TeX-mode-hook 'enable-paredit-mode)
+(add-hook 'bibtex-mode-hook 'enable-paredit-mode)
 
 (defun truncate-lines ()
   (setq fci-handle-truncate-lines nil)
@@ -19,3 +20,4 @@
 (add-hook 'LaTeX-mode-hook 'truncate-lines)
 (add-hook 'tex-mode-hook 'truncate-lines)
 (add-hook 'TeX-mode-hook 'truncate-lines)
+(add-hook 'bibtex-mode-hook 'truncate-lines)
