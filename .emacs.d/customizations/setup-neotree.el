bea44d3743b234bff65d77c854c12b9e22fc301d
António Nuno Monteiro <anmonteiro@gmail.com>
2016-07-24 12:55:57 +0100

    tweak neotree config

--- .emacs.d/customizations/setup-neotree.el
+++ .emacs.d/customizations/setup-neotree.el
@@ -1,22 +1,31 @@
 ;; Neotree
 ;; https://github.com/jaypei/emacs-neotree
 
-;;  every time when the neotree window is  opened, it will try to find current
-;;  file and jump to node.
-;;(setq-default neo-smart-open t)
+(require 'neotree)
+
+;; every time when the neotree window is  opened, it will try to find current
+;; file and jump to node.
+(setq-default neo-smart-open t)
 
 ;; Don't allow neotree to be the only open window
 (setq-default neo-dont-be-alone t)
 
-
-(require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
-
 
 (setq neo-theme 'nerd) ; 'classic, 'nerd, 'ascii, 'arrow
 
 (setq neo-vc-integration '(face char))
 
-(setq neo-show-hidden-files 't)
+(setq neo-show-hidden-files t)
+
+(setq neo-auto-indent-point t)
+
+;; face customizations
+
+(set-face-attribute 'neo-vc-edited-face nil
+                    :foreground "#E2C08D")
+
+(set-face-attribute 'neo-vc-added-face nil
+                    :foreground "green4")
 
 (provide 'setup-neotree)
