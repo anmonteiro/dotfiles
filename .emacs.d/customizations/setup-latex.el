0783b77828a88b8f6fb355d9e4efde5b345787e9
António Nuno Monteiro <anmonteiro@gmail.com>
2016-08-31 17:58:32 +0100

    autocomplete curlies in latex

--- .emacs.d/customizations/setup-latex.el
+++ .emacs.d/customizations/setup-latex.el
@@ -10,6 +10,28 @@
 (add-hook 'TeX-mode-hook 'enable-paredit-mode)
 (add-hook 'bibtex-mode-hook 'enable-paredit-mode)
 
+(add-hook 'latex-mode-hook
+          (lambda ()
+            (define-key latex-mode-map "{" #'paredit-open-curly)
+            (define-key latex-mode-map "}" #'paredit-close-curly)))
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (define-key LaTeX-mode-map "{" #'paredit-open-curly)
+            (define-key LaTeX-mode-map "}" #'paredit-close-curly)))
+(add-hook 'tex-mode-hook
+          (lambda ()
+            (define-key tex-mode-map "{" #'paredit-open-curly)
+            (define-key tex-mode-map "}" #'paredit-close-curly)))
+(add-hook 'TeX-mode-hook
+          (lambda ()
+            (define-key TeX-mode-map "{" #'paredit-open-curly)
+            (define-key TeX-mode-map "}" #'paredit-close-curly)))
+(add-hook 'bibtex-mode-hook
+          (lambda ()
+            (define-key bibtex-mode-map "{" #'paredit-open-curly)
+            (define-key bibtex-mode-map "}" #'paredit-close-curly)))
+
+
 (defun truncate-lines ()
   (setq fci-handle-truncate-lines nil)
   (setq truncate-partial-width-windows nil)
