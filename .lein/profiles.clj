7b9567f24334308d4350d486263f68113215e361
António Nuno Monteiro <anmonteiro@gmail.com>
2016-09-19 14:31:14 +0100

    also exclude clojure from piggieback in lein's repl profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,2 @@
-{:dev {:dependencies [[com.cemerick/piggieback "0.2.1" :exclusions [org.clojure/clojurescript]]]}}
+{:dev {:dependencies [[com.cemerick/piggieback "0.2.1" :exclusions [org.clojure/clojure
+                                                                    org.clojure/clojurescript]]]}}
