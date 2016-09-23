6f8a9cbb9e1e77c7e0cc7a55fbbe6044281f4629
António Nuno Monteiro <anmonteiro@gmail.com>
2016-09-23 14:21:46 +0200

    spell check markdown files

--- .emacs.d/customizations/setup-markdown.el
+++ .emacs.d/customizations/setup-markdown.el
@@ -10,3 +10,5 @@
             (setq truncate-lines nil)))
 
 (setq markdown-command "pandoc -c file:///Users/anmonteiro/Documents/github/dotfiles/.emacs.d/github-pandoc.css --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
+
+(add-hook 'markdown-mode-hook 'flyspell-mode)
