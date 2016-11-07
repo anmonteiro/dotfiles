128cf4e1d0ac5c22b93557215607d7efed402bb9
António Nuno Monteiro <anmonteiro@gmail.com>
2016-11-07 11:02:07 +0100

    don't truncate lines in markdown anymore

--- .emacs.d/customizations/setup-markdown.el
+++ .emacs.d/customizations/setup-markdown.el
@@ -3,12 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
 
-(add-hook 'markdown-mode-hook
-          (lambda ()
-            (setq fci-handle-truncate-lines nil)
-            (setq truncate-partial-width-windows nil)
-            (setq truncate-lines nil)))
-
 (setq markdown-command "pandoc -c file:///Users/anmonteiro/Documents/github/dotfiles/.emacs.d/github-pandoc.css --from markdown_github -t html5 --mathjax --highlight-style pygments --standalone")
 
 (add-hook 'markdown-mode-hook 'flyspell-mode)
