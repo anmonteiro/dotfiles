6c9af95dfb44a67d782bf326c99927a006015562
António Nuno Monteiro <anmonteiro@gmail.com>
2017-07-02 16:55:00 -0700

    stage 2 optimizations in attempting to speed up emacs

--- .emacs.d/customizations/hacks.el
+++ .emacs.d/customizations/hacks.el
@@ -1,3 +1,7 @@
 (defalias 'find-if 'cl-find-if)
 
+;; Misc attempts to make Emacs not slow down
+(setq font-lock-maximum-decoration 6)
+(setq-default bidi-display-reordering nil)
+
 (provide 'hacks)
