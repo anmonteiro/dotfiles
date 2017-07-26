e47caaa20a2e6d86745ada4cd13f6d832be83378
António Nuno Monteiro <anmonteiro@gmail.com>
2017-07-25 19:45:54 -0700

    helm-kill-ring, tweak `ace-window`'s keybinding

--- .emacs.d/customizations/navigation.el
+++ .emacs.d/customizations/navigation.el
@@ -26,7 +26,7 @@
 (projectile-global-mode)
 
 (require 'ace-window)
-(global-set-key (kbd "M-p") 'ace-window)
+(global-set-key (kbd "M-P") 'ace-window)
 
 ;; Most configuration taken from https://tuhdo.github.io/helm-intro.html
 (require 'helm-config)
@@ -35,6 +35,7 @@
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
 (setq helm-truncate-lines t)
 
@@ -60,7 +61,6 @@
 (helm-projectile-on)
 
 ;; TODO:
-;; - helm-kill-ring
 ;; - helm-occur / swoop
 ;; - helm-google-suggest C-x c C-c g is too long
 ;; - helm-ag
