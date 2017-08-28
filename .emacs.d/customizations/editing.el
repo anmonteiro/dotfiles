349f4d231fa01114f4d0280f34726a4c2145831a
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2017-08-27 18:25:35 -0700

    company-complete with C-M-i

--- .emacs.d/customizations/editing.el
+++ .emacs.d/customizations/editing.el
@@ -21,7 +21,8 @@
      ;; (define-key company-active-map (kbd "M-n") nil)
      ;; (define-key company-active-map (kbd "M-p") nil)
      (define-key company-active-map (kbd "C-n") 'company-select-next)
-     (define-key company-active-map (kbd "C-p") 'company-select-previous)))
+     (define-key company-active-map (kbd "C-p") 'company-select-previous)
+     (define-key company-mode-map (kbd "C-M-i") 'company-complete)))
 
 (setq company-idle-delay 0.3
       company-tooltip-idle-delay 0.3)
--- .emacs.d/customizations/setup-clojure.el
+++ .emacs.d/customizations/setup-clojure.el
@@ -91,8 +91,7 @@
      (define-key clojure-mode-map (kbd "C-c C-v") 'cider-start-http-server)
      (define-key clojure-mode-map (kbd "C-M-r") 'cider-refresh)
      (define-key clojure-mode-map (kbd "C-c u") 'cider-user-ns)
-     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)
-     (define-key cider-mode-map (kbd "C-M-i") 'company-complete)))
+     (define-key cider-mode-map (kbd "C-c u") 'cider-user-ns)))
 
 (add-hook 'cider-mode-hook #'company-mode t)
 (add-hook 'cider-mode-hook #'cider-company-enable-fuzzy-completion t)
