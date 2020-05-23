d3ecd5f2fbbbad8445bb72b393aead6958f10b7e
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-05-23 15:41:58 -0700

    tweak xmonad / xmobar to work with a second screen

--- nix/xmonad/xmonad.hs
+++ nix/xmonad/xmonad.hs
@@ -6,12 +6,14 @@ import XMonad
 
 import XMonad.Actions.Navigation2D
 import XMonad.Config
+import XMonad.Hooks.DynamicBars
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 -- import XMonad.Layout.BinarySpacePartition
 import XMonad.Layout.FixedColumn
 import XMonad.Layout.Spacing
 import XMonad.Layout.Grid
+import XMonad.Layout.IndependentScreens
 import XMonad.Layout.NoBorders
 import XMonad.Layout.MultiToggle
 
@@ -25,11 +27,7 @@ import qualified Data.Map as M
 
 main :: IO ()
 main = do
-  xmobarPipe <- spawnPipe xmobarCommand
-  xmonad
-    -- $ withNavigation2DConfig def { layoutNavigation = [("BSP", hybridNavigation)] }
-    $ myConfig { logHook = dynamicLogWithPP $ myXmobarPP xmobarPipe }
-
+  xmonad myConfig
 
 -- TODO: Get these colors from xrdb
 -- backgroundColor   = "#FEFEFE"
@@ -46,42 +44,68 @@ myConfig = def
   , modMask = mod4Mask
   , terminal = "kitty"
   -- { focusedBorderColor = foregroundColor
-  -- , handleEventHook    = docksEventHook
-  -- , layoutHook         = avoidStruts $ spacingWithEdge 4 emptyBSP
+  , handleEventHook    = docksEventHook
+                         <+> dynStatusBarEventHook xmobarCreate xmobarDestroy
+                         <+> handleEventHook def
+
+  , logHook            = myLogHook
   , layoutHook         = myLayoutHook
   , manageHook         = manageDocks
   -- , normalBorderColor  = middleColor
   , workspaces         = myWorkspaces
   , keys          = \c -> myKeys c `M.union` keys def c
+  , startupHook   = docksStartupHook
+                    >> dynStatusBarStartup xmobarCreate xmobarDestroy
+                    >> startupHook def
   }
 
 myWorkspaces = ["1:work", "2:web", "3:msg", "4", "5", "6"]
+-- myWorkspaces = withScreens 2 ["1:work", "2:web", "3:msg", "4", "5", "6"]
+
+togglevga = do
+  screencount <- countScreens
+  if screencount > 1
+   then spawn "xrandr --output DP1 --off"
+   else spawn "xrandr --output DP1 --auto --above eDP1"
+   -- "xrandr --output VGA1 --auto --right-of LVDS1"
+
+toggleBluetooth = do
+  spawn "if [[ $(bluetoothctl show | awk -F': ' '/Powered:/ { print $2 }') == 'no' ]]; \
+\    then bluetoothctl power on; \
+\    else bluetoothctl power off; \
+\  fi"
 
 myKeys (XConfig {modMask = modm}) = M.fromList $
   [ ((0, xK_Print), spawn "maim -s \"/tmp/screenshot-$(date -Iseconds).png\"")
+  , ((0, xK_F7), togglevga)
+  , ((0, xK_F10), toggleBluetooth)
   -- ((modm , xK_x), spawn "xlock")
   ]
 
-myXmobarPP xmobarPipe = def
+myLogHook = multiPP myXmobarPP myXmobarPP
+
+myXmobarPP = def
   { ppCurrent         = pad . xmobarColor foregroundColor  ""
   , ppHidden          = pad . xmobarColor middleColor ""
   , ppHiddenNoWindows = pad . xmobarColor middleColor ""
   , ppLayout          = const ""
-  , ppOutput          = hPutStrLn xmobarPipe
   , ppTitle           = const ""
   , ppVisible         = pad . xmobarColor middleColor ""
   , ppWsSep           = " "
   }
 
-xmobarCommand :: String
-xmobarCommand =
-  intercalate " "
+xmobarCreate :: DynamicStatusBar
+xmobarCreate (S sid) = spawnPipe $ intercalate " "
     [ "xmobar"
     , "-d"
     , "-B", stringed backgroundColor
     , "-F", stringed middleColor
+    , "--screen", stringed (show sid)
     ]
       where stringed x = "\"" ++ x ++ "\""
+
+xmobarDestroy :: DynamicStatusBarCleanup
+xmobarDestroy = return ()
 
 -- with spacing
 myLayoutHook = ((spacingRaw False (Border 30 0 0 0) True (Border 0 0 0 0) False)
