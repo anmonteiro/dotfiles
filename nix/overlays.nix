bcd9b6b919b2ea30a72e7d923c5554e012a9a2cd
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2020-09-26 15:01:19 -0700

    update

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,8 +1,8 @@
 
 let
   overlays = builtins.fetchTarball {
-    url = https://github.com/anmonteiro/nix-overlays/archive/ddc82904.tar.gz;
-    sha256 = "0ngbg6q64gswvqbbsgwgyxy470a88bxy5b2m4zn611cpzqn5ih1z";
+    url = https://github.com/anmonteiro/nix-overlays/archive/1e18d3b.tar.gz;
+    sha256 = "02jl1vdyili1yihl9zmc23snvmka202ll2ch6fwksn4fms1p28kw";
   };
 
 in import overlays
