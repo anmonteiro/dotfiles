6ae4c724d6937deb04b4c52d9d95c5aa9a1812bd
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-11-14 21:01:59 -0800

    update flake lock

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,11 +1,8 @@
 {
   description = "Piaf Nix Flake";
 
-  # inputs.flake-utils.url = "github:numtide/flake-utils";
-  # inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
-  inputs.anmonteiro.url = "/home/anmonteiro/projects/nix-overlays";
+  inputs.anmonteiro.url = "github:anmonteiro/nix-overlays";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
-  # inputs.anmonteiro.url = "github:anmonteiro/nix-overlays";
 
   outputs = { self, nixpkgs, anmonteiro, nixos-hardware }:
     let system = "x86_64-linux";
