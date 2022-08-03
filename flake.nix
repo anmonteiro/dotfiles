892a505f426f976d641cb0749ba7566ff0f597b2
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-08-03 13:31:33 -0700

    flake updates

--- flake.nix
+++ flake.nix
@@ -1,19 +1,20 @@
 {
   description = "Piaf Nix Flake";
 
-  inputs.anmonteiro.url = "github:anmonteiro/nix-overlays";
+  inputs.nixpkgs.url = "github:anmonteiro/nix-overlays";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware";
+  inputs.nixos-hardware.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, anmonteiro, nixos-hardware }:
+  outputs = { self, nixpkgs, nixos-hardware }:
     let system = "x86_64-linux";
     in
-    {
+    rec {
       legacyPackages = nixpkgs.legacyPackages;
       nixosConfigurations = import ./nixos-configurations.nix
         {
           inherit nixos-hardware;
           nixosSystem = nixpkgs.lib.nixosSystem;
-          pkgs = anmonteiro.legacyPackages."${system}";
+          pkgs = legacyPackages."${system}";
         };
     };
 }
