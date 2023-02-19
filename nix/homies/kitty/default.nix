e611c18a1be3c0db380440eec5d9623c9015c48b
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-02-19 12:16:49 -0800

    wrap kitty app on macOS, don't ask for confirmation closing

--- nix/homies/kitty/default.nix
+++ nix/homies/kitty/default.nix
@@ -1,10 +1,17 @@
-{kitty, symlinkJoin, makeWrapper}:
+{ kitty, symlinkJoin, makeWrapper, stdenv }:
+
+let conf = ./kitty.conf;
+in
+
 symlinkJoin {
   name = "kitty";
   buildInputs = [makeWrapper];
   paths = [ kitty ];
   postBuild = ''
-    wrapProgram "$out/bin/kitty" --add-flags "--config ${./kitty.conf}"
+    ${if stdenv.isDarwin then ''
+        wrapProgram "$out/Applications/kitty.app/Contents/MacOS/kitty" --add-flags "--config ${conf}"
+      '' else ""}
+    wrapProgram "$out/bin/kitty" --add-flags "--config ${conf}"
   '';
 }
 
