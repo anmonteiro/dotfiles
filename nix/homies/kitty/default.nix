885e0ba51a6aff81fbc8639a887cf4ea79b16c17
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-08-26 14:36:48 -0700

    patch kitty for 0.36 on macOS

--- nix/homies/kitty/default.nix
+++ nix/homies/kitty/default.nix
@@ -1,12 +1,20 @@
 { kitty, symlinkJoin, makeWrapper, stdenv }:
 
-let conf = ./kitty.conf;
+let
+  conf = ./kitty.conf;
+  kittyPatched = kitty.overrideAttrs (_: {
+    postPatch = ''
+      mkdir fonts
+      cp ${./SymbolsNerdFontMono-Regular.ttf} fonts/SymbolsNerdFontMono-Regular.ttf
+    '';
+  });
+
 in
 
 symlinkJoin {
   name = "kitty";
-  buildInputs = [makeWrapper];
-  paths = [ kitty ];
+  buildInputs = [ makeWrapper ];
+  paths = [ kittyPatched ];
   postBuild = ''
     ${if stdenv.isDarwin then ''
         wrapProgram "$out/Applications/kitty.app/Contents/MacOS/kitty" --add-flags "--config ${conf}"
@@ -14,4 +22,3 @@ symlinkJoin {
     wrapProgram "$out/bin/kitty" --add-flags "--config ${conf}"
   '';
 }
-
