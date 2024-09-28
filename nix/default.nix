ed6b99636e8c44e926be949b81a8d0ae88039107
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-09-29 00:20:46 +0100

    cleanup zshrc, update prompt function

--- nix/default.nix
+++ nix/default.nix
@@ -5,8 +5,8 @@ let
   flake = (import
     (
       fetchTarball {
-        url = https://github.com/edolstra/flake-compat/archive/b4a3401.tar.gz;
-        sha256 = "1qc703yg0babixi6wshn5wm2kgl5y1drcswgszh4xxzbrwkk9sv7";
+        url = https://github.com/edolstra/flake-compat/archive/0f9255e01.tar.gz;
+        sha256 = "0m9grvfsbwmvgwaxvdzv6cmyvjnlww004gfxjvcl806ndqaxzy4j";
       }
     )
     { src = ./..; }
