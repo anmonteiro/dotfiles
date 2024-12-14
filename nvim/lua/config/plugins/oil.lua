4a83d80a885ca94c97e9af2f7a26f6c7d76de842
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-12-13 18:50:35 -0800

    add oil

---
+++ nvim/lua/config/plugins/oil.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      CustomOilBar = function()
+        local path = vim.fn.expand "%"
+        path = path:gsub("oil://", "")
+
+        return "  " .. vim.fn.fnamemodify(path, ":.")
+      end
+
+      require("oil").setup {
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<C-l>"] = false,
+          ["<C-k>"] = false,
+          ["<C-j>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        win_options = {
+          winbar = "%{v:lua.CustomOilBar()}",
+        },
+        view_options = {
+          show_hidden = true,
+          --[[ is_always_hidden = function(name, _)
+            local folder_skip = { "dev-tools.locks", "dune.lock", "_build" }
+            return vim.tbl_contains(folder_skip, name)
+          end, ]]
+        },
+      }
+
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  },
+}
+
