ac03724a586ffef1e791b0756d17e28c096e9b11
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2025-03-16 15:02:59 -0700

    wip

--- nix/homies/kitty/default.nix
+++ nix/homies/kitty/default.nix
@@ -12,7 +12,7 @@ in
 symlinkJoin {
   name = "kitty";
   nativeBuildInputs = [ makeBinaryWrapper autoSignDarwinBinariesHook ];
-  paths = [ kitty ];
+  paths = [ (kitty.overrideAttrs (_: { doCheck = false; })) ];
   postBuild = ''
     ${if stdenv.isDarwin then ''
         wrapProgram "$out/Applications/kitty.app/Contents/MacOS/kitty" --add-flags "--config ${conf}"
