24213c0213b1d951141f2c4770626ab6786d1893
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-04-05 13:54:24 -0700

    feat(nvim): add format-on-save toggles

--- nvim/lua/config/plugins/formatting.lua
+++ nvim/lua/config/plugins/formatting.lua
@@ -9,6 +9,8 @@ return {
           javascript = { "prettier", lsp_format = "prefer" },
           typescript = { "prettier", lsp_format = "prefer" },
           typescriptreact = { "prettier", lsp_format = "prefer" },
+          json = { "prettier", lsp_format = "prefer" },
+          jsonc = { "prettier", lsp_format = "prefer" },
           css = { "prettier", lsp_format = "prefer" },
           scss = { "prettier", lsp_format = "prefer" },
           sql = { "pg_format", lsp_format = "prefer" },
@@ -16,6 +18,7 @@ return {
           nix = { "nixfmt", lsp_format = "never" },
         },
         format_on_save = function(bufnr)
+          if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return nil end
           if not vim.bo[bufnr].modifiable or vim.bo[bufnr].buftype ~= "" then return nil end
 
           return {
@@ -46,6 +49,28 @@ return {
             command = "nixfmt",
           },
         },
+      })
+
+      vim.api.nvim_create_user_command("FormatDisable", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        bang = true,
+        desc = "Disable autoformat-on-save (use ! for current buffer only)",
+      })
+
+      vim.api.nvim_create_user_command("FormatEnable", function(args)
+        if args.bang then
+          vim.b.disable_autoformat = false
+        else
+          vim.g.disable_autoformat = false
+        end
+      end, {
+        bang = true,
+        desc = "Enable autoformat-on-save (use ! for current buffer only)",
       })
     end,
   },
