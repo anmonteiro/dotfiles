bdc9684c180c93b2ba10a3a2f4ec77d01c160722
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2026-04-19 11:02:53 -0700

    update neovim tests

--- nix/homies/common-packages.nix
+++ nix/homies/common-packages.nix
@@ -13,7 +13,19 @@ let
 
   # Tmux with a custom tmux.conf baked in
   tmux = callPackage ./tmux { };
-  neovimWithPython = pkgs.neovim.override {
+  # Neovim's treesitter functional tests on Darwin can collide on short `--listen`
+  # names when XDG_RUNTIME_DIR falls back to a long temp path.
+  patchedNeovimUnwrapped = pkgs.neovim.unwrapped.overrideAttrs (old: {
+    preCheck = lib.concatStringsSep "\n" [
+      (old.preCheck or "")
+      ''
+        export XDG_RUNTIME_DIR="$NIX_BUILD_TOP/xdg-runtime"
+        mkdir -p "$XDG_RUNTIME_DIR"
+        chmod 700 "$XDG_RUNTIME_DIR"
+      ''
+    ];
+  });
+  neovimWithPython = pkgs.wrapNeovim patchedNeovimUnwrapped {
     withPython3 = true;
     extraPython3Packages =
       ps: with ps; [
